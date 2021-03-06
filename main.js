f84f1009c35bda1e2047b088109a41f702ec6637
Guest <guest-yqw7tu@h26817.sgb.skir.kr>
2021-03-06 22:46:18 +0900

    storage

--- main.js
+++ main.js
@@ -1,10 +1,58 @@
 class Main{
     constructor(){
-        window.addEventListener("resize",this.resize.bind(this));
+       this.menuSelect();
     }
 
-    resize(){
+    menuSelect(){
+        $('#menu')
+        .on('mouseover',(e)=>{
+            $('#menu ul li').on('mouseover',(e)=>{
+                let item = Number(e.target.dataset.th);
+                let widthval;
+                switch(item){
+                    case 0:
+                        widthval = '48px';
+                        break;
+                    case 1:
+                        widthval = '56px';
+                        break;
+                    case 2:
+                        widthval = '45px';
+                        break;
+                    case 3:
+                        widthval = '57px';
+                        break;
+                    case 4:
+                        widthval = '64px';
+                        break;
+                }
         
-    }
+                $('#menuOnMouse').css('width', `${widthval}`);
+            });
 
+            let curcol = Math.floor($('#menu').width() / 70) -1;
+            let barPos= Math.floor((e.clientX-230)/70);
+            
+            barPos = barPos <= curcol ? barPos: curcol;
+
+            let pos = barPos * 70;
+
+            $('#menuOnMouse')
+            .css('background-color','white')
+            .css('left',`${pos}px`);
+
+        })
+        .on('mousemove',()=>{
+            $('#menuOnMouse').css('transition','width 0.3s ease-in-out,left 0.3s ease-in-out');
+        })
+        .on('mouseleave',()=>{
+            $('#menuOnMouse')
+            .css('background-color','rgba(0,0,0,0)')  
+            .css('transition','width 0s ease-in-out,left 0s ease-in-out');
+        })
+    }
+}
+
+window.onload = () => {
+    new Main();
 }
