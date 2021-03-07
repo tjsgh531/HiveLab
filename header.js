05acda50e5049b6c03b41f97a7b22ba8d8e75a00
Guest <guest-cxy6vr@h26817.sgb.skir.kr>
2021-03-07 23:21:13 +0900

    storage

--- header.js
+++ header.js
@@ -28,30 +28,27 @@ export class Header{
     menuSelect(){
         $('#menu > ul ')
         .on('mouseover',(e)=>{
-            $('#menu > ul > li').on('mouseover',(e)=>{
-                let item = Number(e.target.dataset.th);
-                let widthval;
-                switch(item){
-                    case 0:
-                        widthval = '48px';
-                        break;
-                    case 1:
-                        widthval = '56px';
-                        break;
-                    case 2:
-                        widthval = '45px';
-                        break;
-                    case 3:
-                        widthval = '57px';
-                        break;
-                    case 4:
-                        widthval = '64px';
-                        break;
-                }
+            let item = Number(e.target.dataset.th);
+            let widthval;
+            switch(item){
+                case 0:
+                    widthval = '44px';
+                    break;
+                case 1:
+                    widthval = '52px';
+                    break;
+                case 2:
+                    widthval = '40px';
+                    break;
+                case 3:
+                    widthval = '50px';
+                    break;
+                case 4:
+                    widthval = '59px';
+                    break;
+            }
+            $('#menuOnMouse').css('width', `${widthval}`);
         
-                $('#menuOnMouse').css('width', `${widthval}`);
-            });
-
             let menuWidth = $('#menu > ul').width();
             let maxColNum = Math.floor(menuWidth/70);
             let maxMousPos = (maxColNum-1) * 70;
@@ -61,7 +58,6 @@ export class Header{
             $('#menuOnMouse')
             .css('background-color','white')
             .css('left',`${pos}px`);
-            
         })
         .on('mousemove',()=>{
             $('#menuOnMouse').css('transition','width 0.3s ease-in-out,left 0.3s ease-in-out');
